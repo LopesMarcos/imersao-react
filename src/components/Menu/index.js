58c4858bb192f8ca91789444a3efca606fa68e32
LopesMarcos <marcosvalterlopesjunior@gmail.com>
2020-07-31 22:37:18 -0300

    Aula 2.6

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-            <a href="/">    
+            <Link to="/">    
             <img className="Logo" src={Logo} alt="NobodyFlix"/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink " href="/">
+            <Button as={Link} className="ButtonLink " to="/cadastro/video">
                 Novo vídeo
             </Button>  
 
